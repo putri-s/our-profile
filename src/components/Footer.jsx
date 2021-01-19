bb59b1eb1ad02b37ab0e0d723e0241b6206f25ed
putri-s <dazai2730@icloud.com>
2021-01-19 22:07:40 +0700

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return(
-    <footer class="bg-color1 lighten-4 center flow-text page-footer">
-      <p class="white-text">Website sedang dalam tahap pengembangan</p>
+    <footer class="bg-color1 lighten-4 center flow-text ">
+      <p class="white-text">Our Profile, 2021. Putri Shefia Andini. </p>
     </footer>
   );
 };
